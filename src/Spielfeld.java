8787b2183014928d2c163806686f6d33b7553985
David <david.wildi@gmx.ch>
2018-10-11 13:45:37 +0200

    + Spielfeld

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -3,15 +3,23 @@ import java.util.ArrayList;
 public class Spielfeld {
 	
 	private int anzFelder;
+	private String space = "  ";
 	private Zelle zelle = new Zelle();
 	private ArrayList<Zelle> zellenArray = new ArrayList<Zelle>();
+	Random random = new Random();
 	
 	public void setSpielfeld() {
-		
+		anzFelder = 7;
+		for (int i = 0; i <= anzFelder; i++) {
+			System.out.print(space + i + space);
+		}
+		for (int j = 0; j <= anzFelder; j++) {
+			System.out.println("\n" + j);
+		}
 	}
 	
 	public void setBomben() {
-		
+
 	}
 
 }
